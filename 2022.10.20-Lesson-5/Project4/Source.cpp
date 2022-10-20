c9c486dac6235ff6de813869e5660431197935ae
021213 <st021213@spbu.ru>
2022-10-20 10:34:27 +0300

    Lesson 5 changes

--- 2022.10.20-Lesson-5/Project4/Source.cpp
+++ 2022.10.20-Lesson-5/Project4/Source.cpp
@@ -19,8 +19,9 @@ int main(int argc, char* argv[])
 	//new int[n] == (int*)malloc(n * sizeof(int))
 	//  delete[] == free(a)
 
-	/*C style*/
+	/*C style*/	
 	a = (int*)malloc(n * sizeof(int));//a = (int*)calloc(n, sizeof(int));
+	std::cout << a << std::endl;
 	if (a != NULL)
 	{
 		for (int i = 0; i < n; ++i)
@@ -31,6 +32,13 @@ int main(int argc, char* argv[])
 		{
 			std::cout << *(a + i) << " "; 
 		}
+		std::cout << std::endl;
+		a = (int*)realloc(a, 2 * n * sizeof(int));
+		for (int i = 0; i < 2 * n; ++i)
+		{
+			std::cout << *(a + i) << " ";
+		}
+		std::cout << std::endl;
 		free(a);
 	}
 
